f61f6e603eaeb875e532cb2405056347f418a0be
Cristhian2430 <82115251+Cristhian2430@users.noreply.github.com>
2024-05-12 22:47:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import json
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 st.title('Control de Pagos')
-
+monto_meta = 5500
 with open(r"/mount/src/control_dp/control.json", 'r') as file:
     data = json.load(file)
 
@@ -21,6 +22,9 @@ df['Monto Acumulado'] = df['monto'].cumsum()
 st.bar_chart(df, x="fecha", y="Monto Acumulado")
 st.line_chart(df, x="fecha", y="Monto Acumulado")
 
-
+fig = px.line(df, x='fecha', y='Monto Acumulado', title='Evolución del Monto Acumulado')
+fig.add_hline(y = monto_meta, line_dash="dash", line_color="red", annotation_text="Meta de monto", 
+              annotation_position="top right")
+st.plotly_chart(fig, use_container_width=True)
 
 st.write(df.head())
