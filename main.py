0c70e12040bee855e1e430f9d79ec1ac583b4953
Cristhian2430 <82115251+Cristhian2430@users.noreply.github.com>
2024-05-12 22:28:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 st.title('Grupo 1 - PUCP Python')
 st.write('¡Hola!')
 
-with open(r"/mount/src/Control_DP/control.json", 'r') as file:
+with open(r"/mount/src/control_dp/control.json", 'r') as file:
     data = json.load(file)
 
 df = pd.DataFrame(list(data.items()), columns=['fecha', 'monto'])
